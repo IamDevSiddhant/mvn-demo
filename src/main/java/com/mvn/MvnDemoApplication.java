34e3637dd0eccb1930750ad38a1f5fc7c9bec132
IamDevSiddhant <siddhantpawar572@gmail.com>
2023-12-12 09:27:28 +0530

    initial Commit

---
+++ src/main/java/com/mvn/MvnDemoApplication.java
@@ -0,0 +1,14 @@
+package com.mvn;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnDemoApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Spring!!");
+
+	}
+
+}
